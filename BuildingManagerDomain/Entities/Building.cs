03efed0116ee57131e2058fed8c73fd6e2215fa6
Rafael <AN262843@fi365.ort.edu.uy>
2024-04-07 15:08:47 -0300

    GREEN AddIdToBuilding

---
+++ BuildingManagerDomain/Entities/Building.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BuildingManagerDomain.Entities
+{
+    public class Building
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ BuildingManagerDomainTest/BuildingTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BuildingManagerDomain.Entities;
+
+namespace BuildingManagerDomainTest
+{
+    [TestClass]
+    public class BuildingTest
+    {
+        [TestMethod]
+        public void BuildingIdTest()
+        {
+            Guid buildingId = Guid.NewGuid();
+            Building building = new Building { Id = buildingId };
+            Assert.AreEqual(buildingId, building.Id);
+        }
+    }
+}
