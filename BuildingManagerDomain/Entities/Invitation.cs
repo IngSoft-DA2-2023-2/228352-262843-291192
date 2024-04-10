617fabf67392746439298ae08f0c6b77aab74cf2
Franca Chechi <fchiqui01@gmail.com>
2024-04-09 21:00:50 -0300

    GREEN AddIdToInvitation

---
+++ BuildingManagerDomain/Entities/Invitation.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace BuildingManagerDomain.Entities
+{
+    public class Invitation
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ BuildingManagerDomainTest/InvitationTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BuildingManagerDomain.Entities;
+
+namespace BuildingManagerDomainTest
+{
+    [TestClass]
+    public class InvitationTest
+    {
+        [TestMethod]
+        public void InvitationIdTest()
+        {
+            Guid invitationId = Guid.NewGuid();
+            Invitation invitation = new() { Id = invitationId };
+            Assert.AreEqual(invitationId, invitation.Id);
+        }
+    }
+}
