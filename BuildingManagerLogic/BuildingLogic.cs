b15358be749af20243a11942854e4a082afbc9ba
Rafael <AN262843@fi365.ort.edu.uy>
2024-04-12 01:27:34 -0300

    GREEN CreateBuildingTest_Ok

---
+++ BuildingManagerLogic/BuildingLogic.cs
@@ -0,0 +1,26 @@
+﻿using BuildingManagerDomain.Entities;
+using BuildingManagerIDataAccess;
+using BuildingManagerILogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingManagerLogic
+{
+    public class BuildingLogic : IBuildingLogic
+    {
+        private IBuildingRepository _buildingRepository;
+
+        public BuildingLogic(IBuildingRepository buildingRepository)
+        {
+            _buildingRepository = buildingRepository;
+        }
+
+        public Building CreateBuilding(Building building)
+        {
+            return _buildingRepository.CreateBuilding(building);
+        }
+    }
+}
