49770e90fc1839f6045f9ef564f839f9dc4c133d
juantejer4 <68563891+juantejer4@users.noreply.github.com>
2024-04-14 20:21:31 -0300

    GREEN CreateCategoryOk

---
+++ BuildingManagerApi/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BuildingManagerILogic;
+using Microsoft.AspNetCore.Mvc;
+using BuildingManagerModels.Outer;
+
+namespace BuildingManagerApi.Controllers
+{
+    public class CategoryController: ControllerBase
+    {
+        private readonly ICategoryLogic _categoryLogic;
+
+        public CategoryController(ICategoryLogic categoryLogic)
+        {
+            _categoryLogic = categoryLogic;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory([FromBody] string categoryName)
+        {
+            CreateCategoryResponse createCategoryResponse = new CreateCategoryResponse(_categoryLogic.CreateCategory(categoryName));
+            return CreatedAtAction(nameof(CreateCategory), createCategoryResponse);
+        }
+    }
+}
---
+++ BuildingManagerApiTest/Controllers/CategoryControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using BuildingManagerApi.Controllers;
+using BuildingManagerDomain.Entities;
+using BuildingManagerILogic;
+using BuildingManagerILogic.Exceptions;
+using BuildingManagerModels.CustomExceptions;
+using BuildingManagerModels.Inner;
+using BuildingManagerModels.Outer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BuildingManagerApiTest.Controllers
+{
+    [TestClass]
+    public class CategoryControllerTest
+    {
+        [TestMethod]
+        public void CreateCategory_Ok()
+        {
+            var category = new Category { 
+                Id = Guid.NewGuid(), 
+                Name  = "Test"
+            };
+            var mockCategoryLogic = new Mock<ICategoryLogic>(MockBehavior.Strict);
+            mockCategoryLogic.Setup(x => x.CreateCategory(It.IsAny<string>())).Returns(category);
+            var controller = new CategoryController(mockCategoryLogic.Object);
+
+            var result = controller.CreateCategory("Test");
+            var createdAtActionResult = result as CreatedAtActionResult;
+            var content = createdAtActionResult.Value as CreateCategoryResponse;
+
+            mockCategoryLogic.VerifyAll();
+            Assert.AreEqual(new CreateCategoryResponse(category), content);
+        }
+    }
+}
