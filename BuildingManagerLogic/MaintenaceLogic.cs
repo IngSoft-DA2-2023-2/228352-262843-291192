7991a7fd512a209e4fe36353fce27f4f862c6384
juantejer4 <68563891+juantejer4@users.noreply.github.com>
2024-04-17 02:09:11 -0300

    GREEN CreateMaintenanceStaffOk

---
+++ BuildingManagerLogic/MaintenaceLogic.cs
@@ -0,0 +1,20 @@
+﻿using BuildingManagerDomain.Entities;
+using BuildingManagerIDataAccess;
+using BuildingManagerILogic;
+
+namespace BuildingManagerLogic
+{
+    public class MaintenaceLogic : IMaintenanceLogic
+    {
+        private readonly IMaintenanceRepository _maintenanceRepository;
+        public MaintenaceLogic(IMaintenanceRepository maintenanceRepository)
+        {
+            _maintenanceRepository = maintenanceRepository;
+        }
+        public MaintenanceStaff CreateMaintenanceStaff(MaintenanceStaff maintenanceStaff)
+        {
+
+            return _maintenanceRepository.CreateMaintenanceStaff(maintenanceStaff);
+        }
+    }
+}
