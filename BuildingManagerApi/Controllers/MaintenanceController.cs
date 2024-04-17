0c2767d18141ac79de3fa9fbbf984347a70ed25f
juantejer4 <68563891+juantejer4@users.noreply.github.com>
2024-04-17 01:41:14 -0300

    GREEN CreateMaintenaceStaffOk

---
+++ BuildingManagerApi/Controllers/MaintenanceController.cs
@@ -0,0 +1,26 @@
+﻿using BuildingManagerILogic;
+using BuildingManagerModels.Inner;
+using BuildingManagerModels.Outer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingManagerApi.Controllers
+{
+    [ApiController]
+    [Route("api/maintenances")]
+    public class MaintenanceController : ControllerBase
+    {
+        private readonly IMaintenanceLogic _maintenanceLogic;
+
+        public MaintenanceController(IMaintenanceLogic maintenanceLogic)
+        {
+            _maintenanceLogic = maintenanceLogic;
+        }
+
+        [HttpPost]
+        public IActionResult CreateMaintenanceStaff([FromBody] CreateMaintenanceStaffRequest maintenanceStaffRequest)
+        {
+            CreateMaintenanceStaffResponse createMaintenanceStaffResponse = new CreateMaintenanceStaffResponse(_maintenanceLogic.CreateMaintenanceStaff(maintenanceStaffRequest.ToEntity()));
+            return CreatedAtAction(nameof(CreateMaintenanceStaff), createMaintenanceStaffResponse);
+        }
+    }
+}
---
+++ BuildingManagerApiTest/Controllers/MaintenanceControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using BuildingManagerApi.Controllers;
+using BuildingManagerDomain.Entities;
+using BuildingManagerILogic;
+using BuildingManagerILogic.Exceptions;
+using BuildingManagerModels.CustomExceptions;
+using BuildingManagerModels.Inner;
+using BuildingManagerModels.Outer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BuildingManagerApiTest.Controllers
+{
+    [TestClass]
+    public class MaintenanceControllerTest
+    {
+        private MaintenanceStaff _maintenaceStaff;
+        private CreateMaintenanceStaffRequest _createMaintenanceStaffRequest;
+        private CreateMaintenanceStaffResponse _createMaintenanceStaffResponse;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _maintenaceStaff = new MaintenanceStaff
+            {
+                Id = new Guid(),
+                Name = "John",
+                Lastname = "Doe",
+                Email = "john@abc.com",
+                Password = "pass123"
+            };
+            _createMaintenanceStaffRequest = new CreateMaintenanceStaffRequest
+            {
+                Name = "John",
+                Lastname = "Doe",
+                Email = "john@abc.com",
+                Password = "pass123"
+            };
+            _createMaintenanceStaffResponse = new CreateMaintenanceStaffResponse(_maintenaceStaff);
+
+        }
+        [TestMethod]
+        public void CreateMaintenanceStaff_Ok()
+        {
+            var mockMaintenanceLogic = new Mock<IMaintenanceLogic>(MockBehavior.Strict);
+            mockMaintenanceLogic.Setup(x => x.CreateMaintenanceStaff(It.IsAny<MaintenanceStaff>())).Returns(_maintenaceStaff);
+            var maintenanceController = new MaintenanceController(mockMaintenanceLogic.Object);
+
+            var result = maintenanceController.CreateMaintenanceStaff(_createMaintenanceStaffRequest);
+            var createdAtActionResult = result as CreatedAtActionResult;
+            var content = createdAtActionResult.Value as CreateMaintenanceStaffResponse;
+
+            mockMaintenanceLogic.VerifyAll();
+            Assert.AreEqual(_createMaintenanceStaffResponse, content);
+        }
+    }
+}
