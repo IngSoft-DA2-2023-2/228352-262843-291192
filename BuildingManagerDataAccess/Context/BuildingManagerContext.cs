127addea2a8fd693eec9f71acd07a510a5c60b85
Rafael <AN262843@fi365.ort.edu.uy>
2024-04-20 12:41:35 -0300

    GREEN CreateBuildingTest

--- BuildingManagerDataAccess/Context/BuildingManagerContext.cs
+++ BuildingManagerDataAccess/Context/BuildingManagerContext.cs
@@ -3,14 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BuildingManagerDataAccess.Context
 {
-    public class BuildingManagerContext: DbContext
+    public class BuildingManagerContext : DbContext
     {
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<MaintenanceStaff> MaintenanceStaff { get; set; }
+        public DbSet<Building> Buildings { get; set; }
 
         public BuildingManagerContext(DbContextOptions<BuildingManagerContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Apartment>().HasKey(a => new { a.Floor, a.Number });
+            modelBuilder.Entity<Owner>().HasKey(o => o.Email);
         }
     }
 }
