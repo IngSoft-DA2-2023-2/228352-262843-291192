ff2bf56acf472dabb417a50161261bfbcf2b71b6
Franca Chechi <fchiqui01@gmail.com>
2024-04-28 21:13:02 -0300

    GREEN AddInProgressRequestsToMaintenanceData

--- BuildingManagerDomain/Entities/MaintenanceData.cs
+++ BuildingManagerDomain/Entities/MaintenanceData.cs
@@ -3,12 +3,14 @@ namespace BuildingManagerDomain.Entities
 {
     public struct MaintenanceData
     {
-        public MaintenanceData(int openRequests, int closeRequests)
+        public MaintenanceData(int openRequests, int closeRequests, int inProgressRequests)
         {
             OpenRequests = openRequests;
             CloseRequests = closeRequests;
+            InProgressRequests = inProgressRequests;
         }
         public int OpenRequests { get; }
         public int CloseRequests { get; }
+        public int InProgressRequests { get; }
     }
 }
--- BuildingManagerDomainTest/MaintenanceData.cs
+++ BuildingManagerDomainTest/MaintenanceData.cs
@@ -11,7 +11,10 @@ namespace BuildingManagerDomainTest
         {
             int openRequests = 5;
             int closeRequests = 5;
-            MaintenanceData data = new(openRequests, closeRequests);
+            int inProgressRequests = 6;
+
+            MaintenanceData data = new(openRequests, closeRequests, inProgressRequests);
+
             Assert.AreEqual(openRequests, data.OpenRequests);
         }
 
@@ -20,8 +23,23 @@ namespace BuildingManagerDomainTest
         {
             int openRequests = 5;
             int closeRequests = 5;
-            MaintenanceData data = new(openRequests, closeRequests);
+            int inProgressRequests = 6;
+
+            MaintenanceData data = new(openRequests, closeRequests, inProgressRequests);
+
             Assert.AreEqual(closeRequests, data.CloseRequests);
+        }
+
+        [TestMethod]
+        public void MaintenanceDataInProgressRequestsTest()
+        {
+            int openRequests = 5;
+            int closeRequests = 5;
+            int inProgressRequests = 6;
+
+            MaintenanceData data = new(openRequests, closeRequests, inProgressRequests);
+            
+            Assert.AreEqual(inProgressRequests, data.InProgressRequests);
         }
     }
 }
