e59d7eabbd6eb359745a600282a295def2297cd0
Franca Chechi <fchiqui01@gmail.com>
2024-04-29 20:52:31 -0300

    GREEN GetReportSuccessfully

--- BuildingManagerLogic/ReportLogic.cs
+++ BuildingManagerLogic/ReportLogic.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
 using BuildingManagerDomain.Entities;
+using BuildingManagerIDataAccess;
 using BuildingManagerILogic;
 
 namespace BuildingManagerLogic
 {
     public class ReportLogic : IReportLogic
     {
+        private readonly IRequestRepository _requestRepository;
+        public ReportLogic(IRequestRepository requestRepository)
+        {
+            _requestRepository = requestRepository;
+        }
         public List<MaintenanceData> GetReport(Guid buildingId)
         {
-            throw new System.NotImplementedException();
+            MaintenanceReport report = new MaintenanceReport(_requestRepository);
+            return report.GetReport(buildingId);
         }
     }
 }
---
+++ BuildingManagerLogicTest/ReportLogicTest.cs
@@ -0,0 +1,65 @@
+using BuildingManagerDomain.Entities;
+using BuildingManagerDomain.Enums;
+using BuildingManagerIDataAccess;
+using BuildingManagerIDataAccess.Exceptions;
+using BuildingManagerILogic.Exceptions;
+using BuildingManagerLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BuildingManagerLogicTest
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class ReportLogicTest
+    {
+        [TestMethod]
+        public void GetReportSuccessfully()
+        {
+            List<Request> requests =
+            [new Request()
+                {
+                    Id = new Guid(),
+                    Description = "description",
+                    CategoryId = new Guid("11111111-1111-1111-1111-111111111111"),
+                    BuildingId = new Guid("11111111-1111-1111-1111-111111111111"),
+                    ApartmentFloor = 1,
+                    ApartmentNumber = 1,
+                    State = RequestState.OPEN,
+                    MaintainerId = new Guid("11111111-1111-1111-1111-111111111111"),
+                },
+                new Request()
+                {
+                    Id = new Guid(),
+                    Description = "description",
+                    CategoryId = new Guid("11111111-1111-1111-1111-111111111111"),
+                    BuildingId = new Guid("11111111-1111-1111-1111-111111111111"),
+                    ApartmentFloor = 1,
+                    ApartmentNumber = 1,
+                    State = RequestState.CLOSE,
+                    MaintainerId = new Guid("11111111-1111-1111-1111-111111111111"),
+                },
+                new Request()
+                {
+                    Id = new Guid(),
+                    Description = "description",
+                    CategoryId = new Guid("11111111-1111-1111-1111-111111111111"),
+                    BuildingId = new Guid("11111111-1111-1111-1111-111111111111"),
+                    ApartmentFloor = 1,
+                    ApartmentNumber = 1,
+                    State = RequestState.PENDING,
+                    MaintainerId = new Guid("11111111-1111-1111-1111-111111111111"),
+                }
+            ];
+            var requestRepositoryMock = new Mock<IRequestRepository>(MockBehavior.Strict);
+            requestRepositoryMock.Setup(x => x.GetRequests()).Returns(requests);
+            List<MaintenanceData> data = [new MaintenanceData(1, 1, 1, 0, "")];
+            var report = new ReportLogic(requestRepositoryMock.Object);
+
+            var result = report.GetReport(new Guid("11111111-1111-1111-1111-111111111111"));
+            requestRepositoryMock.VerifyAll();
+            Assert.AreEqual(data.First(), result.First());
+        }
+    }
+}
