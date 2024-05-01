49c6654cbe9f0a630d635474dc326d83207f1c0b
Rafael <AN262843@fi365.ort.edu.uy>
2024-04-30 23:13:51 -0300

    GREEN CategoriesReportDataCloseRequestsTest

---
+++ BuildingManagerDomain/Entities/CategoriesReportData.cs
@@ -0,0 +1,18 @@
+﻿namespace BuildingManagerDomain.Entities
+{
+    public class CategoriesReportData
+    {
+        public string CategoryName { get; set; }
+        public int OpenRequests { get; set; }
+        public int CloseRequests { get; set; }
+        public int InProgressRequests { get; set; }
+
+        public CategoriesReportData(string categoryName, int openRequests, int closeRequests, int inProgressRequests)
+        {
+            CategoryName = categoryName;
+            OpenRequests = openRequests;
+            CloseRequests = closeRequests;
+            InProgressRequests = inProgressRequests;
+        }
+    }
+}
---
+++ BuildingManagerDomainTest/CategoriesReportDataTest.cs
@@ -0,0 +1,21 @@
+﻿using BuildingManagerDomain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildingManagerDomainTest
+{
+    [TestClass]
+    public class CategoriesReportDataTest
+    {
+        [TestMethod]
+        public void CategoriesReportDataCloseRequestsTest()
+        {
+            string categoryName = "Electricista";
+            int openRequests = 5;
+            int closeRequests = 2;
+            int inProgressRequests = 1;
+
+            CategoriesReportData data = new(categoryName, openRequests, closeRequests, inProgressRequests);
+            Assert.AreEqual(closeRequests, data.CloseRequests);
+        }
+    }
+}
