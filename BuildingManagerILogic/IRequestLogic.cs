e6158c5d6d31b0d25000ef3068dc410a3d0c910a
juantejer4 <68563891+juantejer4@users.noreply.github.com>
2024-05-01 03:23:04 -0300

    GREEN attend request api

--- BuildingManagerILogic/IRequestLogic.cs
+++ BuildingManagerILogic/IRequestLogic.cs
@@ -7,6 +7,7 @@ namespace BuildingManagerILogic
     public interface IRequestLogic
     {
         Request AssignStaff(Guid id, Guid maintenanceStaffId);
+        Request AttendRequest(Guid id);
         public Request CreateRequest(Request request);
         public List<Request> GetRequests();
     }
