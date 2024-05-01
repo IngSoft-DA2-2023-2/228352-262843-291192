a80940d05d0041beff24e9ec61c470cdef85de3b
Franca Chechi <fchiqui01@gmail.com>
2024-04-30 21:54:18 -0300

    GREEN MaintenanceReportDataInProgressRequestsTest

--- BuildingManagerDomain/Entities/MaintenanceReportData.cs
+++ BuildingManagerDomain/Entities/MaintenanceReportData.cs
@@ -5,12 +5,14 @@ namespace BuildingManagerDomain.Entities
 {
     public struct MaintenanceReportData
     {
-        public MaintenanceReportData(int openRequests, int closeRequests)
+        public MaintenanceReportData(int openRequests, int closeRequests, int inProgressRequests)
         {
             OpenRequests = openRequests;
             CloseRequests = closeRequests;
+            InProgressRequests = inProgressRequests;
         }
         public int OpenRequests { get; }
         public int CloseRequests { get; }
+        public int InProgressRequests { get; }
     }
 }
