9834523456a63a0398c8b9a18c9e9c4d90ab80d9
Franca Chechi <fchiqui01@gmail.com>
2024-05-25 15:39:28 -0300

    migration for new discriminator

---
+++ Backend/BuildingManagerDataAccess/Migrations/20240525182924_constructionCompanyAdminDiscriminator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildingManagerDataAccess.Migrations
+{
+    public partial class constructionCompanyAdminDiscriminator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
