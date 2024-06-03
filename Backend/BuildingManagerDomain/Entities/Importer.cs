7b3c6ff6905b43c9d704ce789638bafbfa016102
juantejer4 <68563891+juantejer4@users.noreply.github.com>
2024-06-02 22:48:50 -0300

    GREEN Create Importer With Name

---
+++ Backend/BuildingManagerDomain/Entities/Importer.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildingManagerDomain.Entities
+{
+    public class Importer
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Backend/BuildingManagerDomainTest/ImporterTest.cs
@@ -0,0 +1,20 @@
+﻿using BuildingManagerDomain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildingManagerDomainTest
+{
+    [TestClass]
+    public class ImporterTest
+    {
+        [TestMethod]
+        public void CreateImporterWithName_SetsNameProperty()
+        {
+            var expectedName = "TestImporter";
+
+            var importer = new Importer { Name = expectedName };
+
+            Assert.AreEqual(expectedName, importer.Name);
+        }
+
+    }
+}
