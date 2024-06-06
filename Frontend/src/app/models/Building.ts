60e5de9cd69ea1fbdc90bb456a054e465297b715
Rafael <AN262843@fi365.ort.edu.uy>
2024-06-05 22:52:03 -0300

    add: Se agrega nombre de manager en la lista de edificios

--- Frontend/src/app/models/Building.ts
+++ Frontend/src/app/models/Building.ts
@@ -2,10 +2,12 @@ export class Building{
     constructor(
         public name: string,
         public address: string,
+        public manager: string
     ){}
 }
 
 export interface Building{
     name: string;
     address: string;
+    manager: string;
 }
