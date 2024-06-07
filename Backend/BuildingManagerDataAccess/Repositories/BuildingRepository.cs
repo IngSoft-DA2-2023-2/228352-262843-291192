9837c12cc290f6bba54508ac71d689b5d31c66db
Rafael <AN262843@fi365.ort.edu.uy>
2024-06-07 20:08:10 -0300

    GREEN: Se debe poder ver detalles de un edificio sin manager

--- Backend/BuildingManagerDataAccess/Repositories/BuildingRepository.cs
+++ Backend/BuildingManagerDataAccess/Repositories/BuildingRepository.cs
@@ -247,10 +247,11 @@ namespace BuildingManagerDataAccess.Repositories
                                         .Include(b => b.Apartments)
                                             .ThenInclude(a => a.Owner)
                                         .First(b => b.Id == buildingId);
-            Manager manager = _context.Set<User>().First(u => u.Id == building.ManagerId) as Manager;
+            
             ConstructionCompany constructionCompany = _context.Set<ConstructionCompany>().First(cc => cc.Id == building.ConstructionCompanyId);
-            if (manager != null)
+            if (_context.Set<Manager>().Any(u => u.Id == building.ManagerId))
             {
+                Manager manager = _context.Set<Manager>().First(u => u.Id == building.ManagerId);
                 return new BuildingDetails(building.Id, building.Name, building.Address, building.Location, (decimal)building.CommonExpenses, (Guid)building.ManagerId, manager.Name, building.ConstructionCompanyId, constructionCompany.Name, building.Apartments);
             }
             return new BuildingDetails(building.Id, building.Name, building.Address, building.Location, (decimal)building.CommonExpenses, Guid.Empty, "", building.ConstructionCompanyId, constructionCompany.Name, building.Apartments);
