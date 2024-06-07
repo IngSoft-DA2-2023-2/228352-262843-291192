c781f24ba469dff0a792c0065c1c1b766fcf4235
Franca Chechi <fchiqui01@gmail.com>
2024-06-07 17:55:37 -0300

    GREEN BuildingAddressTest

--- Backend/JsonImporter/JsonBuilding.cs
+++ Backend/JsonImporter/JsonBuilding.cs
@@ -6,11 +6,6 @@ namespace JsonImporter
     public class JsonBuilding
     {
         public string Name { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            var other = (JsonBuilding)obj;
-            return Name == other.Name ;
-        }
+        public string Address { get; set; }
     }
 }
--- Backend/JsonImporterTest/JsonBuildingTest.cs
+++ Backend/JsonImporterTest/JsonBuildingTest.cs
@@ -12,5 +12,13 @@ namespace JsonImporterTest
             JsonBuilding building = new JsonBuilding { Name = name };
             Assert.AreEqual(name, building.Name);
         }
+
+        [TestMethod]
+        public void BuildingAddressTest()
+        {
+            string address = "some address";
+            JsonBuilding building = new JsonBuilding { Address = address };
+            Assert.AreEqual(address, building.Address);
+        }
     }
 }
