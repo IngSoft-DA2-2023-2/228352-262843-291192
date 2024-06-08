b760ba3d71d1c3704a7edef9a463934008ba329e
juantejer4 <68563891+juantejer4@users.noreply.github.com>
2024-06-08 02:46:03 -0300

    remove connectedUser from local storage when login out

--- Frontend/src/app/components/sidebar/sidebar.component.ts
+++ Frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { UserRole } from '../../models/UserRole';
+import { User } from '../../models/User';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIf],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
@@ -21,6 +24,27 @@ export class SidebarComponent {
 
   logout() {
     localStorage.removeItem('sessionToken');
+    localStorage.removeItem('connectedUser');
     this.router.navigate(['/login']);
+  }
+
+  isAdmin() {
+    const connectedUser = JSON.parse(localStorage.getItem('connectedUser') as string) as User;
+    return connectedUser.role === UserRole.ADMIN;
+  }
+
+  isManager() {
+    const connectedUser = JSON.parse(localStorage.getItem('connectedUser') as string) as User;
+    return connectedUser.role === UserRole.MANAGER;
+  }
+
+  isMaintenance() {
+    const connectedUser = JSON.parse(localStorage.getItem('connectedUser') as string) as User;
+    return connectedUser.role === UserRole.MAINTENANCE;
+  }
+
+  isCompanyAdmin() {
+    const connectedUser = JSON.parse(localStorage.getItem('connectedUser') as string) as User;
+    return connectedUser.role === UserRole.CONSTRUCTIONCOMPANYADMIN;
   }
 }
