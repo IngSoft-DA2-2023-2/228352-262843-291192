df125d1c2075986c9e841d82bee43aabd775b2a0
Franca Chechi <fchiqui01@gmail.com>
2024-06-09 16:33:02 -0300

    GREEN ImportWithDuplicatedBuildings

--- Backend/BuildingManagerLogicTest/ImporterLogicTest.cs
+++ Backend/BuildingManagerLogicTest/ImporterLogicTest.cs
@@ -1,6 +1,7 @@
 using BuildingManagerDomain.Entities;
 using BuildingManagerIDataAccess;
 using BuildingManagerIImporter;
+using BuildingManagerIImporter.Exceptions;
 using BuildingManagerILogic;
 using BuildingManagerILogic.Exceptions;
 using BuildingManagerLogic;
@@ -158,7 +159,7 @@ namespace BuildingManagerLogicTest
                 }
             };
             List<Building> buildings = new List<Building> { building };
-            string importerName = "DefaultJson";
+            string importerName = "InvalidImporter";
             string data = "{\"edificios\":[{\"nombre\":\"Lastorres\",\"direccion\":{\"calle_principal\":\"Av.6deDiciembre\",\"numero_puerta\":3030,\"calle_secundaria\":\"Av.EloyAlfaro\"},\"encargado\":null,\"gps\":{\"latitud\":-0.176,\"longitud\":-78.48},\"gastos_comunes\":5000,\"departamentos\":[{\"piso\":1,\"numero_puerta\":101,\"habitaciones\":3,\"conTerraza\":false,\"baños\":2,\"propietarioEmail\":\"juan.perez@gmail.com\"}]}]}";
 
             var userLogicMock = new Mock<IUserLogic>(MockBehavior.Strict);
@@ -171,7 +172,7 @@ namespace BuildingManagerLogicTest
             Exception exception = null;
             try
             {
-                importerLogic.ImportData("InvalidImporter", data, sessionToken);
+                importerLogic.ImportData(importerName, data, sessionToken);
             }
             catch (Exception ex)
             {
@@ -182,6 +183,71 @@ namespace BuildingManagerLogicTest
             companyLogicMock.VerifyAll();
             buildingLogicMock.VerifyAll();
             Assert.IsInstanceOfType(exception, typeof(NotFoundException));
+        }
+
+        [TestMethod]
+        public void ImportWithDuplicatedBuildings()
+        {
+            Guid sessionToken = Guid.NewGuid();
+            Guid ccadminId = Guid.NewGuid();
+            Guid companyId = Guid.NewGuid();
+            Guid managerId = Guid.NewGuid();
+            Guid buildingId = Guid.NewGuid();
+            Owner owner = new Owner()
+            {
+                Email = "juan.perez@gmail.com",
+                LastName = "lastname",
+                Name = "name"
+            };
+            Building building = new Building
+            {
+                Id = buildingId,
+                ManagerId = managerId,
+                Name = "Lastorres",
+                Address = "Av.6deDiciembre 3030, Av.EloyAlfaro",
+                Location = "(-0.176,-78.48)",
+                ConstructionCompanyId = companyId,
+                CommonExpenses = 5000,
+                Apartments = new List<Apartment>
+                {
+                    new Apartment
+                    {
+                        BuildingId =buildingId,
+                        Floor = 1,
+                        Number = 101,
+                        Rooms = 3,
+                        Bathrooms = 2,
+                        HasTerrace = false,
+                        Owner = owner
+                    }
+                }
+            };
+            List<Building> buildings = new List<Building> { building };
+            string importerName = "DefaultJson";
+            string data = "{\"edificios\":[{\"nombre\":\"Lastorres\",\"direccion\":{\"calle_principal\":\"Av.6deDiciembre\",\"numero_puerta\":3030,\"calle_secundaria\":\"Av.EloyAlfaro\"},\"encargado\":null,\"gps\":{\"latitud\":-0.176,\"longitud\":-78.48},\"gastos_comunes\":5000,\"departamentos\":[{\"piso\":1,\"numero_puerta\":101,\"habitaciones\":3,\"conTerraza\":false,\"baños\":2,\"propietarioEmail\":\"juan.perez@gmail.com\"}]},{\"nombre\":\"Lastorres\",\"direccion\":{\"calle_principal\":\"Av.6deDiciembre\",\"numero_puerta\":3030,\"calle_secundaria\":\"Av.EloyAlfaro\"},\"encargado\":null,\"gps\":{\"latitud\":-0.176,\"longitud\":-78.48},\"gastos_comunes\":5000,\"departamentos\":[{\"piso\":1,\"numero_puerta\":101,\"habitaciones\":3,\"conTerraza\":false,\"baños\":2,\"propietarioEmail\":\"juan.perez@gmail.com\"}]}]}";
+            var userLogicMock = new Mock<IUserLogic>(MockBehavior.Strict);
+            var companyLogicMock = new Mock<IConstructionCompanyLogic>(MockBehavior.Strict);
+            var buildingLogicMock = new Mock<IBuildingLogic>(MockBehavior.Strict);
+            var importerLogic = new ImporterLogic(userLogicMock.Object, companyLogicMock.Object, buildingLogicMock.Object);
+            userLogicMock.Setup(x => x.GetUserIdFromSessionToken(It.IsAny<Guid>())).Returns(ccadminId);
+            companyLogicMock.Setup(x => x.GetCompanyIdFromUserId(It.IsAny<Guid>())).Returns(companyId);
+            buildingLogicMock.Setup(x => x.GetOwnerFromEmail(It.IsAny<string>())).Returns(owner);
+            userLogicMock.Setup(x => x.GetManagerIdFromEmail(It.IsAny<string>())).Returns(managerId);
+
+            Exception exception = null;
+            try
+            {
+                importerLogic.ImportData(importerName, data, sessionToken);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            userLogicMock.VerifyAll();
+            companyLogicMock.VerifyAll();
+            buildingLogicMock.VerifyAll();
+            Assert.IsInstanceOfType(exception, typeof(DuplicatedValueException));
         }
     }
 }
