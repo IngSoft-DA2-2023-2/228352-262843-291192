46082193ee1a3412dff0f8945dd171863b2ad114
Franca Chechi <fchiqui01@gmail.com>
2024-06-09 15:53:33 -0300

    GREEN ListImportersNamesSuccessfully

--- Backend/BuildingManagerLogicTest/ImporterLogicTest.cs
+++ Backend/BuildingManagerLogicTest/ImporterLogicTest.cs
@@ -34,5 +34,29 @@ namespace BuildingManagerLogicTest
             Assert.AreEqual(importers[0].Name, result[0].Name);
             Assert.AreEqual(importers[1].Name, result[1].Name);
         }
+
+        [TestMethod]
+        public void ListImportersNamesSuccessfully()
+        {
+            List<IImporter> importers = new List<IImporter>();
+            Mock<IImporter> mockJsonImporter = new Mock<IImporter>(MockBehavior.Strict);
+            mockJsonImporter.Setup(x => x.Name).Returns("DefaultJson");
+            Mock<IImporter> mockXmlImporter = new Mock<IImporter>(MockBehavior.Strict);
+            mockXmlImporter.Setup(x => x.Name).Returns("DefaultXml");
+            importers.Add(mockJsonImporter.Object);
+            importers.Add(mockXmlImporter.Object);
+            var userLogicMock = new Mock<IUserLogic>(MockBehavior.Strict);
+            var companyLogicMock = new Mock<IConstructionCompanyLogic>(MockBehavior.Strict);
+            var buildingLogicMock = new Mock<IBuildingLogic>(MockBehavior.Strict);
+            var importerLogic = new ImporterLogic(userLogicMock.Object, companyLogicMock.Object, buildingLogicMock.Object);
+
+            var result = importerLogic.ListImportersNames();
+
+            userLogicMock.VerifyAll();
+            companyLogicMock.VerifyAll();
+            buildingLogicMock.VerifyAll();
+            Assert.AreEqual(importers[0].Name, result[0]);
+            Assert.AreEqual(importers[1].Name, result[1]);
+        }
     }
 }
